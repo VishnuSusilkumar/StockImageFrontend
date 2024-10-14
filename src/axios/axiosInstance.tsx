b551132916bf4c7c017a4386a871bbb2eb86ce58
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-10-14 11:50:02 +0530

    base url updated

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,11 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://stock-image-backend.onrender.com/api' 
-    : 'http://localhost:8080/api',
-  withCredentials: true,  
+  baseURL: "https://stock-image-backend.onrender.com/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
-
